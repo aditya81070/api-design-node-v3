7809efcdbf96bb1ee00d13d4953c6e4fa8164996
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-26 22:16:59 +0530

    complete lesson1

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
-
+const router = express.Router()
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,4 +12,26 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+  res.send({ message: 'hello from nested' })
+})
+
+app.use('/api', router)
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'okay, got it.' })
+})
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Listening at port 3000')
+  })
+}
